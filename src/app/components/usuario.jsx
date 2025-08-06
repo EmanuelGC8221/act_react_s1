6c821bf1ce24cdefc56386d25f2bc1b87f9ba164
EmanuelGC <yoemanuelgiraldo@gmail.com>
2025-08-06 15:52:26 -0500

    mostrar multiples usuarios

--- src/app/components/usuario.jsx
+++ src/app/components/usuario.jsx
@@ -1,12 +1,21 @@
 'use client';
 
 import React from 'react';
-function Usuario(props) {
-    return (
-        <div className='bg-gray-100 p-4 m-2 rounded-lg'>
-            <h2 className='text-xl font-bold'>{props.nombre}</h2>
-        </div>
 
-    );
+function Usuario({ usuario, saludar }) {
+  return (
+    <div className="bg-gray-100 p-4 m-2 rounded-lg">
+      <h2 className="text-xl font-bold">Nombre: {usuario.nombre}</h2>
+      <p>Edad: {usuario.edad} años</p>
+      <p>Ciudad: {usuario.ciudad}</p>
+      <button
+        className="bg-blue-500 text-white px-4 py-2 mt-2 rounded hover:bg-blue-600"
+        onClick={() => saludar(usuario.nombre)}
+      >
+        Saludar
+      </button>
+    </div>
+  );
 }
+
 export default Usuario;
