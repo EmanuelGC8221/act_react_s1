39603406f677457f87512c3e0e55b3adf3b1e196
EmanuelGC <yoemanuelgiraldo@gmail.com>
2025-08-06 15:31:47 -0500

    componente Usuario

---
+++ src/app/components/usuario.jsx
@@ -0,0 +1,12 @@
+'use client';
+
+import React from 'react';
+function Usuario(props) {
+    return (
+        <div className='bg-gray-100 p-4 m-2 rounded-lg'>
+            <h2 className='text-xl font-bold'>{props.nombre}</h2>
+        </div>
+
+    );
+}
+export default Usuario;
